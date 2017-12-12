b6dfdb15212d7408e217dd777508e68ca1a56202
Rulx Philome ALEXIS <rulxphilome.alexis@wfp.org>
2017-12-12 14:40:48 -0400

    Env Temoin

---
+++ seeEnv.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rulxphilome.alexis
+ * Date: 12/12/2017
+ * Time: 2:08 PM
+ */
+
+echo  $_SERVER['RDS_HOSTNAME']."<br />";
+echo  $_SERVER['RDS_DB_NAME']."<br />";
+echo  $_SERVER['RDS_HOSTNAME']."<br />";
+echo  $_SERVER['RDS_USERNAME']."<br />";
+echo  $_SERVER['RDS_PASSWORD']."<br />";
+
+?>
